316c71a86e03665fc48bbdddedab3a755e36ffb6
Arnulfo Duran Zavala <arnulfo@arnulfos-mac.local>
2019-01-15 13:24:30 -0800

    Initial Commit

---
+++ Rent House/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rent House
+//
+//  Created by Arnulfo on 1/15/19.
+//  Copyright © 2019 Arnulfo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
